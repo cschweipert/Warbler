2e89625ab27d0b773007d01cccdbd6e0c545fe17
Christina Schweipert <44006691+cschweipert@users.noreply.github.com>
2023-03-24 19:10:07 -0600

    add app context

--- seed.py
+++ seed.py
@@ -1,19 +1,21 @@
 """Seed database with sample data from CSV Files."""
 
 from csv import DictReader
-from app import db
+from app import db, app
 from models import User, Message, Follows
 
-db.drop_all()
-db.create_all()
+with app.app_context():
 
-with open('generator/users.csv') as users:
-    db.session.bulk_insert_mappings(User, DictReader(users))
+    db.drop_all()
+    db.create_all()
 
-with open('generator/messages.csv') as messages:
-    db.session.bulk_insert_mappings(Message, DictReader(messages))
+    with open('generator/users.csv') as users:
+        db.session.bulk_insert_mappings(User, DictReader(users))
 
-with open('generator/follows.csv') as follows:
-    db.session.bulk_insert_mappings(Follows, DictReader(follows))
+    with open('generator/messages.csv') as messages:
+        db.session.bulk_insert_mappings(Message, DictReader(messages))
 
-db.session.commit()
+    with open('generator/follows.csv') as follows:
+        db.session.bulk_insert_mappings(Follows, DictReader(follows))
+
+    db.session.commit()
